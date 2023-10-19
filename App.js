0ed0c5ba2be5edcd50fadf062079d048439662f4
gitirom <wadiaromdane10@gmail.com>
2023-10-19 16:20:18 +0200

    Building a simple addTask app

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert,
+        Button,
+        StyleSheet,
+        Text,
+        TextInput,
+        View,
+        ScrollView,
+        FlatList,
+      } from 'react-native';
+
+
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+
+  function goalInputHandler (text) {
+    setEnteredGoalText(text);
+  }
+  function addGoalHandler () {
+    setCourseGoals((currentCourseGoals) =>
+      [...currentCourseGoals, enteredGoalText]
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <View style={styles.appContainer} >
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} 
+        placeholder='Your course gaol!'
+        onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler}/>
+      </View>
+      <View  style={styles.goalsContainer} >
+        <ScrollView  > 
+          {
+            courseGoals.map((goal) => (
+            <View key={goal} style={styles.goalItem}> 
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+            ))
+          }
+        </ScrollView>
+      </View>
     </View>
+    
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "blue",
+  },
+  goalText: {
+    color: 'white',
+  },
+
 });
+
+//scrollView its making as to get scrolling the our view 
+//the view that having key property this for get the radius working in the both devices 
